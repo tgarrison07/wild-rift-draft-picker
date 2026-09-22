77b8653c609e1222b1ddc095b0c3f37c9381f210
tgarrison07 <trumpetoffaith@gmail.com>
2026-09-21 21:47:42 -0400

    Add offline service worker

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE="jungle-draft-v3";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{
+    const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;
+  }).catch(()=>caches.match("./index.html"))));
+});
